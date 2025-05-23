joint text and speech training. with correct number of params, TE:causal false, PE after masking

joint_text_speech/data_asr.py
@@ -53,7 +53,6 @@ class TextDataset(torch.utils.data.Dataset):
         text = self.data[idx]['text']
         inp =  text
         label = text
-        print(text)
         return {"text_trans" : text,
                 "input_ids" : self.bos_token + self.tokenizer.encode(inp),
                 "input_len" : len(self.tokenizer.encode(inp)) + 1,
@@ -146,28 +145,10 @@ def collate_fn_text(batch, tokenizer=None):
             }
 
 
-def load_data(working_directory='data',
-                          name='librispeech',
-                          data_url='/mnt/matylda6/ivendrame/miniLM/data/librispeech_shuf.txt',
-                          train_split_proportion=0.9999,
-                          ):
-
-    with open(data_url, 'r', encoding='utf-8') as f:
-        data = f.read()
-        data = [{'text': f'{_.lower()}'} for _ in data.split('\n') if _ ]
-        #    random.shuffle(data)
-    n = len(data)
-    train_data = data[:int(n * train_split_proportion)]
-    val_data = data[int(n * train_split_proportion):]
-
-    return train_data, val_data
-
-
 def load_from_textfile(textfile_path='/mnt/matylda6/ivendrame/miniLM/data/librispeech_shuf.txt'):
 
     with open(textfile_path, 'r', encoding='utf-8') as f:
-        data = f.read()
-        data = [{'text': f'{_}\n'} for _ in data.split('\n') if _ and len(_.split())< 231]
+        data = [{'text': f'{_.strip().lower()}'} for _ in f.readlines() if _.strip() and len(_.split())< 231]
 
         return data
     

joint_text_speech/models_asr.py
@@ -125,7 +125,7 @@ class TextEncoder(nn.Module):
                  tokenizer,
                  num_layers = 2,
                  dropout = 0,
-                 causal=True
+                 causal=False
                  ):
         super(TextEncoder, self).__init__()
         self.embedding = nn.Embedding(
@@ -156,21 +156,12 @@ class TextEncoder(nn.Module):
             # "input_len": input_len,
             # "labels": labels,
             # "labels_len": labels_len
-            # }
-        print(x['input_ids'].shape)
-        print(x['input_ids'][0])
-        
+            # }      
         features = self.embedding(x['input_ids'])
-        print(features[0])
         text_length = x['input_len']
-        print(text_length[0])
-        features = self.positional_embedding(features)
-        print(features.shape)
         mask = torch.rand((features.shape[0], features.shape[1], 1), device=features.device)
-        print("mask")
-        print(mask[0])
         features = torch.where(mask>self.mask_rate, features, torch.zeros_like(features))
-        print(features[0])
+        features = self.positional_embedding(features)
         for block in self.blocks:
             features = block(features)
 
@@ -301,14 +292,14 @@ class Connector(nn.Module):
                 features = self.cnn[i](features)
                 features = features.transpose(1, 2)
                 features = nn.functional.gelu(features)
-                input_length = (1 + (speech_length - kernel_size) / stride).int()
+                speech_length = (1 + (speech_length - kernel_size) / stride).int()
             features = self.positional_embedding(features)
+            input_length = speech_length
             features = features[:, :input_length.max().item(), :]
         else: #TextEncoder output
             #masked text embeddings with positional embeddings.
             features = x['TextEncoder_output']
             input_length = x['TextEncoder_output_length']
-            # TODO: max lenght limitation for text encoder output also?
         for block in self.blocks:
             features = block(features)
 