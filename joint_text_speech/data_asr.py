wer calculation, resume with text encoder, normalizer, min length filtering, bugs fixed based on pre-training for DST on multiple datasets made on Lumi (only asr)

joint_text_speech/data_asr.py
@@ -12,6 +12,7 @@ from tokenizers.trainers import BpeTrainer
 from tokenizers.decoders import BPEDecoder
 import datasets
 import yaml
+import re
 
 def train_bpe(data, vocab_size=1024, special_tokens=None, output_dir='tokenizer', num_sentences=100000):
     if special_tokens is None:
@@ -101,7 +102,9 @@ class TextDatasetHF(torch.utils.data.Dataset):
         self.data_lengths = {key: len(v) for key,v in data.items()}
         self.tokenizer = tokenizer
         self.bos_token = bos_token
-        self.fake_text_len = 30
+        self.fake_text_len = 5
+        self.cropped_text = 0
+        self.max_text_length = 30
 
     def __len__(self):
         return sum(list(self.data_lengths.values()))
@@ -128,19 +131,27 @@ class TextDatasetHF(torch.utils.data.Dataset):
 
         data_key = self.data[key][idx]
         text = data_key['text'].lower()
+        #text = self.normalizer(text) 
+        text = re.sub(r"[^\w\s']|(?<!\w)'|'(?!\w)", "", text).lower()
         # cut text to max 230 characters
-        if len(text) > 180:
-            text = text[:180]
+        # if len(text) > 180:
+        #     text = text[:180]
         inp = text
         label = text
         inp_encoded = self.tokenizer.encode(inp, add_special_tokens = False)
+        input_ids = [self.bos_token] + inp_encoded
         lab_encoded = self.tokenizer.encode(label, add_special_tokens = False)
-        
+        labels = lab_encoded + [self.tokenizer.eos_token_id]
+        if len(input_ids)>self.max_text_length:
+            input_ids = input_ids[:self.max_text_length]
+            labels = labels[:self.max_text_length]
+            self.cropped_text +=1
+
         return {"text_trans" : text,
-                "input_ids" : [self.bos_token] + inp_encoded,
-                "input_len" : len(inp_encoded) + 1,
-                "labels" : lab_encoded + [self.tokenizer.eos_token_id],
-                "labels_len" : len(lab_encoded) + 1
+                "input_ids" : input_ids,
+                "input_len" : len(input_ids),
+                "labels" : labels,
+                "labels_len" : len(labels)
                 }
 
 class AudioDatasetHF(torch.utils.data.Dataset):
@@ -190,7 +201,8 @@ class AudioDatasetHF(torch.utils.data.Dataset):
          #normalized data structure - if dataset doesn't have it, prep it according to commonvoice_prep.py
         data_key = self.data[key][idx]
         text = data_key['normalized_text'] #if "normalized_text" in data_key else data_key["text"].lower()
-        audio_path = data_key['audio']['path']
+        # print(data_key["audio"])
+        text = re.sub(r"[^\w\s']|(?<!\w)'|'(?!\w)", "", text).lower()
         audio = torch.tensor(data_key['audio']['array'], dtype=torch.float32)
         sample_rate = data_key['audio']['sampling_rate']
         if sample_rate != 16000:
@@ -212,15 +224,24 @@ class AudioDatasetHF(torch.utils.data.Dataset):
         
 
 def load_from_config(ds_type, config_datasets, hf_dataset_path):
-    
     loaded_datasets = {}
     #hf_dataset_path = "/mnt/scratch/tmp/ivendrame/huggingface/modules/datasets_modules/datasets/"
     for k,v in config_datasets[ds_type].items():
         dataset_path = os.path.join(hf_dataset_path, f"prep_{k}_{ds_type}")
-        if os.path.exists(dataset_path):
+        if "path_to_subsets" in v:
+            #dataset_path = os.path.join(dataset_path, v["path_to_subsets"]
+            print(f"loading prepared subsets of dataset {k} from {dataset_path}")
+            #dataset_path = os.path.join(dataset_path, f"prep_{k}_{split}_")
+            for ds in os.listdir(dataset_path):
+                loaded_dataset = datasets.load_from_disk(os.path.join(dataset_path, ds))
+                loaded_datasets[ds]= loaded_dataset
+        
+        elif os.path.exists(dataset_path):
             print(f"loading prepared dataset {k} from {dataset_path}")
             loaded_datasets[k] = datasets.load_from_disk(dataset_path)
-            loaded_datasets[k] = loaded_datasets[k].filter(lambda item: item['audio_len']<(30*16_000), num_proc=4)
+            if 'audio_len' in loaded_datasets[k].features:
+                loaded_datasets[k] = loaded_datasets[k].filter(lambda item: item['audio_len']<(30*16_000), num_proc=4)
+                loaded_datasets[k] = loaded_datasets[k].filter(lambda item: item['audio_len']>(0.5*16_000), num_proc=4)
 
         else:
             print(f"prepared dataset not found {k} in {dataset_path}, taking original dataset and thus filtering audio discarding audios longer then 30s")
@@ -229,11 +250,12 @@ def load_from_config(ds_type, config_datasets, hf_dataset_path):
             ds_split = v["split"]
             #ds_text_column = v["text_column"]
             if ds_subset:
-                loaded_datasets[k] = datasets.load_dataset(ds_name, ds_subset, split = ds_split, trust_remote_code=True, num_proc=2)
+                loaded_datasets[k] = datasets.load_dataset(ds_name, ds_subset, split = ds_split,  num_proc=2) #trust_remote_code=True,
             else:
-                loaded_datasets[k] = datasets.load_dataset(ds_name, split = ds_split, trust_remote_code=True, num_proc=2)
+                loaded_datasets[k] = datasets.load_dataset(ds_name, split = ds_split, num_proc=2)#trust_remote_code=True,
             if 'audio' in loaded_datasets[k].features:
-                loaded_datasets[k] = loaded_datasets[k].filter(lambda item: len(item['audio']["array"])<(30*16_000), num_proc=4)          
+                loaded_datasets[k] = loaded_datasets[k].filter(lambda item: len(item['audio']["array"])<(30*16_000), num_proc=4)
+                loaded_datasets[k] = loaded_datasets[k].filter(lambda item: len(item['audio']["array"])>(0.5*16_000), num_proc=4)          
         
     return loaded_datasets
 

joint_text_speech/dataset_prep.py
@@ -22,6 +22,20 @@ def modify_commonvoice(item, normalizer=None):
     new_item["audio_len"] = len(new_item["audio"]["array"])
     return new_item
 
+def modify_voxpopuli(item, normalizer=None):
+    new_item = {}
+    new_item["text"] = item["normalized_text"]
+    #if ds_text_column == "normalized_text":
+    new_item["normalized_text"] = normalize_text(new_item["text"], normalizer=normalizer)
+    new_item["audio"] = item["audio"]
+    audio = item['audio']['array']
+    audio = torchaudio.transforms.Resample(orig_freq=item['audio']['sampling_rate'], new_freq=16000)(torch.tensor(audio, dtype=torch.float32))
+    new_item["audio"]["sampling_rate"] = 16_000
+    new_item["audio"]["array"] = audio.numpy()
+    new_item["audio_len"] = len(new_item["audio"]["array"])
+    return new_item
+
+
 def modify_librispeech(item, normalizer=None):
     new_item = {}
     new_item["text"] = item["text"]
@@ -106,6 +120,8 @@ def generate_dataset(shards, ds_type, normalizer=None):
                 item = modify_fisher(item, normalizer=normalizer)
             if ds_type.startswith("spokenwoz"):
                 item = modify_spokenwoz(item, normalizer=normalizer)
+            if ds_type == "voxpopuli":
+                item = modify_voxpopuli(item, normalizer=normalizer)
         
             yield {
                 "text": item["text"],            
@@ -132,7 +148,7 @@ def generate_text_ds(shards, ds_type, normalizer=None):
 def main():
     path = "/mnt/matylda6/ivendrame/wavlm_connector_lm/notebooks/test_datasets/"
     dataset_path = "/mnt/scratch/tmp/ivendrame/huggingface/modules/datasets_modules/datasets/"
-    splits = ["text_train"]
+    splits = ["validation"]
     num_proc = 20
     normalizer = EnglishSpellingNormalizer()
 
@@ -141,13 +157,14 @@ def main():
     for split in splits:
         for k,v in config_datasets[str(split)].items():
             ds_name = v["name"].split(":")[0] if ":" in v["name"] else v["name"]
+            print(ds_name)
             ds_subset = v["name"].split(":")[1] if ":" in v["name"] else None
-            ds_split = "train" #v["text_train"]
+            ds_split = v["split"] #v["text_validation"]
             ds_text_column = v["text_column"]
 
             if ds_name == "dialog_studio":
                 for ds in os.listdir(v["path"]):
-                    split = "train"
+                    split = ""
                     loaded_dataset = datasets.load_from_disk(os.path.join(v["path"],ds))
                     loaded_dataset= loaded_dataset[split]
                     shards = []
@@ -163,16 +180,17 @@ def main():
                         "text": datasets.Value("string"),
                         "normalized_text": datasets.Value("string"),
                         }), num_proc=num_proc)
-                    new_dataset.save_to_disk(os.path.join(dataset_path, f"prep_{k}_{split}_{ds}"), )
+                    new_dataset.save_to_disk(os.path.join(dataset_path, f"prep_{k}_text_train_{ds}"), )
             
-            elif ds_name == "open_subtitles":
-                loaded_dataset = datasets.load_dataset(ds_name, split = ds_split, trust_remote_code=True, num_proc=4)
-                    
-                loaded_dataset = loaded_dataset[split]
+            elif k == "open_subtitles":
+                ds_split = "validation"
+                print(ds_name, ds_subset, ds_split)
+                loaded_dataset = datasets.load_dataset(ds_name, "all", split = ds_split, trust_remote_code=True, num_proc=4)
+                loaded_dataset = loaded_dataset
                 shards = []
-                print(f"shards {k} {split}")
+                print(f"shards {k} {ds_split}")
                 for i in range(1):
-                    shard_path = os.path.join(dataset_path, f"{k}_{split}_shard_{i}.json")
+                    shard_path = os.path.join(dataset_path, f"{k}_{ds_split}_shard_{i}.json")
                     shard = loaded_dataset.shard(num_shards=1, index=i)
                     shard.save_to_disk(shard_path)
                     shards.append(shard_path)
@@ -180,23 +198,26 @@ def main():
                 "text": datasets.Value("string"),
                 "normalized_text": datasets.Value("string"),
                 }), num_proc=num_proc)
-                new_dataset.save_to_disk(os.path.join(dataset_path, f"prep_{k}_{split}"), )
+                new_dataset.save_to_disk(os.path.join(dataset_path, f"prep_{k}_{ds_split}"), )
             
             else:
                 if "path" in v:
-                    loaded_dataset = datasets.load_from_disk(v['path'])
-                    loaded_dataset = loaded_dataset["train"]
+                    print("Loading from path", v['path'])
+                    loaded_dataset = datasets.load_from_disk(os.path.join(v['path']))
+
+                    #loaded_dataset = loaded_dataset[ds_split]
                 else:
+                    print("Loading from huggingface", ds_name, ds_subset, ds_split)
                     if ds_subset:
-                        loaded_dataset = datasets.load_dataset(ds_name, ds_subset, split = ds_split, trust_remote_code=True, num_proc=num_proc)
+                        loaded_dataset = datasets.load_dataset(ds_name, ds_subset, split = ds_split, trust_remote_code=True, num_proc=num_proc)#, download_mode='force_redownload' )#download_mode='force_redownload'
                     else:
-                        loaded_dataset = datasets.load_dataset(ds_name, split = ds_split, trust_remote_code=True, num_proc=num_proc)
+                        loaded_dataset = datasets.load_dataset(ds_name, split = ds_split, trust_remote_code=True, num_proc=num_proc)#,download_mode='force_redownload') #  download_mode='force_redownload'
 
                 shards = []
                 print(f"shards {k} {split}")
                 for i in range(8):
                     shard_path = os.path.join(dataset_path, f"{k}_{split}_shard_{i}.json")
-                    shard = loaded_dataset.shard(num_shards=8, index=i)
+                    shard = loaded_dataset.shard(num_shards=10, index=i)
                     shard.save_to_disk(shard_path)
                     shards.append(shard_path)
                 
@@ -207,6 +228,7 @@ def main():
                     "audio": datasets.Audio(sampling_rate=16_000),
                     "audio_len": datasets.Value("int32")}), num_proc=num_proc)
                 new_dataset.save_to_disk(os.path.join(dataset_path, f"prep_{k}_{split}"), )
+                print("Saved dataset to", os.path.join(dataset_path, f"prep_{k}_{split}"))
 
 if __name__ == '__main__':
     main()

joint_text_speech/train_asr.py
@@ -18,11 +18,13 @@ from torch.utils.tensorboard import SummaryWriter
 import transformers
 import datasets
 import accelerate
+from whisper_normalizer.english import EnglishSpellingNormalizer
+
 
 #print environment variables
 print("HF datasets cache", os.environ.get('HF_DATASETS_CACHE', ''))
 print("hf home", os.environ.get('HF_HOME', ''))
-hf_dataset_path = "/mnt/scratch/tmp/ivendrame/huggingface/modules/datasets_modules/datasets/"
+hf_dataset_path = os.environ.get('HF_HOME', '')+ '/modules/datasets_modules/datasets'
 
 def lr_lambda_linear_with_min_lr(step, args):
     peak_lr = args.peak_lr
@@ -182,7 +184,14 @@ def main():
                         help='mask rate for text encoder')
     parser.add_argument('--text_encoder_dim', type=int, default=512,
                         help='text encoder dimension')
-    
+    parser.add_argument('--connector_layers', type = int, default = 2,
+                        help = 'number of connector layers')
+    parser.add_argument('--text_encoder_layers', type = int, default = 2,
+                        help = 'number of text encoder layers')
+    parser.add_argument('--dataset_for_choosing_checkpoint',
+                        help='validation dataset fo choosing the best checkpoint.'
+                        'If unspecified, defaults to the validation dataset in the yaml')
+    parser.add_argument('--mse_loss_logit', action='store_true', help = 'trai with mse loss calculated on logits')
 
     args = parser.parse_args()
     #torch.set_default_dtype(torch.bfloat16)
@@ -198,7 +207,8 @@ def main():
     # device = torch.device('cuda' if torch.cuda.is_available() and not args.no_cuda else 'cpu')
     device = accelerator.device
     print(f'Using device: {device} - index: {accelerator.process_index}')
-    n_gpus = int(len(os.environ.get('CUDA_VISIBLE_DEVICES'))/2)+1
+    # n_gpus = int(len(os.environ.get('CUDA_VISIBLE_DEVICES'))/2)+1
+    n_gpus = accelerator.num_processes
 
     if accelerator.is_main_process:
         writer = SummaryWriter(log_dir=args.log_dir)
@@ -227,6 +237,8 @@ def main():
                 "bos_token": bos_token}
     accelerator.print("lm_config", lm_config)
 
+    best_wer = float('inf')
+    all_metrics = []
     if args.resume:
         accelerator.print('Resuming from checkpoint')
         connector = models_asr.Connector.load_from_dir(os.path.join(args.output_dir, 'latest'), device)
@@ -255,15 +267,21 @@ def main():
         connector = models_asr.Connector.load_from_dir(os.path.join(args.pretrained_dir, 'latest'), device)
         connector = connector.to(device)
         encoder = models_asr.WavLMWrapper.load_from_dir(os.path.join(args.pretrained_dir, 'latest'), device, deactivate_masked_spec_embed = True)
+        if args.text_input:
+            if args.use_llm_emb:
+                text_encoder = models_asr.TextEncoder(args.mask_rate, vocab_size=None, dim_input = args.text_encoder_dim, dim_output = encoder.encoder.config.hidden_size, num_heads=4, ff_size = 4*(args.text_encoder_dim), pad_token=tokenizer.pad_token_id, dim_lm_embeddings=lm.config.hidden_size, num_layers = args.text_encoder_layers)
+            else:
+                text_encoder = models_asr.TextEncoder(args.mask_rate, tokenizer.vocab_size, args.text_encoder_dim, encoder.encoder.config.hidden_size, num_heads=4, ff_size = 4*(args.text_encoder_dim), pad_token=tokenizer.pad_token_id, num_layers = args.text_encoder_layers)
     else:
         print("initialize model parts")
         encoder = models_asr.WavLMWrapper(args.encoder_model_name)
-        connector = models_asr.Connector(encoder.encoder.config.hidden_size, lm.config.hidden_size, num_heads = 4, ff_size = 4*encoder.encoder.config.hidden_size)
-    if args.text_input:
-        if args.use_llm_emb:
-            text_encoder = models_asr.TextEncoder(args.mask_rate, vocab_size=None, dim_input = args.text_encoder_dim, dim_output = encoder.encoder.config.hidden_size, num_heads=4, ff_size = 4*(args.text_encoder_dim), pad_token=tokenizer.pad_token_id, dim_lm_embeddings=lm.config.hidden_size)
-        else:
-            text_encoder = models_asr.TextEncoder(args.mask_rate, tokenizer.vocab_size, args.text_encoder_dim, encoder.encoder.config.hidden_size, num_heads=4, ff_size = 4*(args.text_encoder_dim), pad_token=tokenizer.pad_token_id)
+        connector = models_asr.Connector(encoder.encoder.config.hidden_size, lm.config.hidden_size, num_heads = 4, ff_size = 4*encoder.encoder.config.hidden_size, num_connector_layers = args.connector_layers)
+    
+        if args.text_input:
+            if args.use_llm_emb:
+                text_encoder = models_asr.TextEncoder(args.mask_rate, vocab_size=None, dim_input = args.text_encoder_dim, dim_output = encoder.encoder.config.hidden_size, num_heads=4, ff_size = 4*(args.text_encoder_dim), pad_token=tokenizer.pad_token_id, dim_lm_embeddings=lm.config.hidden_size, num_layers = args.text_encoder_layers)
+            else:
+                text_encoder = models_asr.TextEncoder(args.mask_rate, tokenizer.vocab_size, args.text_encoder_dim, encoder.encoder.config.hidden_size, num_heads=4, ff_size = 4*(args.text_encoder_dim), pad_token=tokenizer.pad_token_id, num_layers = args.text_encoder_layers)
 
     if args.text_input:
         model = models_asr.EncoderConnectorLmWithPretrainedLm(encoder, connector, lm, tokenizer, text_encoder)
@@ -301,6 +319,10 @@ def main():
     val_subsets = {}
     for k, v in dset_valid.items():
         val_subsets[k] = data_asr.AudioDatasetHF({k:v}, tokenizer, bos_token)
+    if args.dataset_for_choosing_checkpoint is None:
+        dataset_for_choosing_checkpoint = list(val_subsets.keys())[-1]
+    else:
+        dataset_for_choosing_checkpoint = args.dataset_for_choosing_checkpoint
     # train dataset
     dset_train_s = data_asr.load_from_config('train', datasets_config, hf_dataset_path)
     train_asr_data = data_asr.AudioDatasetHF(dset_train_s, tokenizer, bos_token)
@@ -406,9 +428,12 @@ def main():
     train_loss = 0
     training_acc = 0
     training_count = 0
-    best_wer = float('inf')
-    all_metrics = []
+    if all_metrics:
+        best_wer = min([m['wer'] for m in all_metrics if m['dataset'] == dataset_for_choosing_checkpoint])
+    else:
+        best_wer = float('inf')
 
+    normalizer = EnglishSpellingNormalizer()
     for j in tqdm.tqdm(range(start_step+1, args.max_steps + 1), disable=not accelerator.is_main_process):
         optimizer.zero_grad()
         if args.connector_eval:
@@ -428,7 +453,7 @@ def main():
                 accelerator.unwrap_model(model).text_encoder.eval()
             else:
                 accelerator.unwrap_model(model).text_encoder.train()
-        
+        train_asr_loader_iter = iter(train_asr_loader)
         ## ACCUMULATION STEPS
         for k in range(args.accumulation_steps):
             try:
@@ -478,10 +503,16 @@ def main():
                                     mse_loss = mse_loss*mask[:, :, None] #bxNxdims
                                     mse_loss = mse_loss.sum() / (mask.sum() * hidden_states_s.shape[-1])
                                     loss_t += args.mse_weight*mse_loss
-                                
+                                if args.mse_loss_logit:
+                                    mse_loss = ((z_s - z_t[0:args.per_device_train_asr_batch_size, padded_len:,:])**2).to(device)
+                                    lengths = x_s_clone['input_len'].to(device)
+                                    mask = models_asr.lengths_to_mask(lengths).to(device) #bxdims
+                                    mse_loss = mse_loss*mask[:, :, None] #bxNxdims
+                                    mse_loss = mse_loss.sum() / (mask.sum() * hidden_states_s.shape[-1])
+                                    loss_t += args.mse_weight*mse_loss
                             else:
                                 z_t, loss_t, acc_t = model(x_t, is_text = True, output_hidden_states=False)
-
+                                loss_t = args.text_weight*loss_t
                             loss = loss_s + loss_t
                         else:
                             loss = loss_s
@@ -496,19 +527,26 @@ def main():
                             z_t, loss_t, acc_t, hidden_states_all_t = model(x_all_t, is_text = True, output_hidden_states=True, last_layer_MSE = args.last_layer_mse)
                             hidden_states_s_t = hidden_states_all_t[0:args.per_device_train_asr_batch_size]
                             padded_len = x_all_t['labels'].shape[1]- x_s_clone['labels'].shape[1]
-
+                            loss_t = args.text_weight*loss_t
                             if args.mse_loss:
                                 mse_loss = ((hidden_states_s - hidden_states_s_t[:,padded_len:,:])**2).to(device) #bxNxdims
                                 lengths = x_s_clone['input_len'].to(device)
                                 mask = models_asr.lengths_to_mask(lengths).to(device) #bxdims
                                 mse_loss = mse_loss*mask[:, :, None] #bxNxdims
                                 mse_loss = mse_loss.sum() / (mask.sum() * hidden_states_s.shape[-1])
-                                loss_t += mse_loss
+                                loss_t += args.mse_weight*mse_loss
+                            if args.mse_loss_logit:
+                                mse_loss = ((z_s - z_t[0:args.per_device_train_asr_batch_size, padded_len:,:])**2).to(device)
+                                lengths = x_s_clone['input_len'].to(device)
+                                mask = models_asr.lengths_to_mask(lengths).to(device) #bxdims
+                                mse_loss = mse_loss*mask[:, :, None] #bxNxdims
+                                mse_loss = mse_loss.sum() / (mask.sum() * hidden_states_s.shape[-1])
+                                loss_t += args.mse_weight*mse_loss
                             
                         else:
                             z_t, loss_t, acc_t = model(x_t, is_text = True, output_hidden_states=False)
-
-                        loss = loss_s + args.text_weight*loss_t
+                            loss_t = args.text_weight*loss_t
+                        loss = loss_s + loss_t
                     else:
                         loss = loss_s
                 
@@ -595,7 +633,9 @@ def main():
 
                         with torch.autocast(enabled = True, device_type = "cuda", dtype= torch.bfloat16):
                             text_x = accelerator.unwrap_model(model).generate(val_x, 100, bos_token = bos_token)
+                            text_x = [normalizer(t) for t in text_x]
                         text_y = val_batch['text_trans']
+                        text_y = [normalizer(t) for t in text_y]
                         all_transcriptions += text_x
                         all_references += text_y
                     val_loss = val_loss / val_count
@@ -680,8 +720,9 @@ def main():
                     if args.train_encoder:
                         accelerator.unwrap_model(model).encoder.save_to_directory(os.path.join(args.output_dir, 'best'))
                     yaml.dump(all_metrics, open(os.path.join(args.output_dir, 'best', 'metrics.yaml'), 'w'))
-            recent_wer = [m['wer'] for m in all_metrics[-args.early_stopping_patience:]]
-            all_wers = [m['wer'] for m in all_metrics]
+            recent_wer = [m['wer'] for m in all_metrics[-args.early_stopping_patience:]
+                          if m["dataset"] == dataset_for_choosing_checkpoint]
+            all_wers = [m['wer'] for m in all_metrics if m["dataset"] == dataset_for_choosing_checkpoint]
 
         if j % args.save_steps == 0 and accelerator.is_main_process:
             accelerator.unwrap_model(model).connector.save_to_directory(os.path.join(args.output_dir, f'checkpoint_{j}'))